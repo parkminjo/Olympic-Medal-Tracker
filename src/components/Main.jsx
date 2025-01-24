a4ef72f5dcfc0c1bc9ce2db269c307993e9a6eec
Minjo Park <consistent.312@gmail.com>
2025-01-24 20:03:21 +0900

    해설 강의 - 정렬 기능 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,26 +9,52 @@ const Main = () => {
     JSON.parse(localStorage.getItem("medalList")) || []
   );
 
+  /** 메달 리스트 정렬 state */
+  const [sortType, setSortType] = useState("gold");
+
   /** medalist 로컬 스토리지에 저장 */
   useEffect(() => {
     localStorage.setItem("medalList", JSON.stringify(medalList));
   }, [medalList]);
 
+  /**
+   * 메달 리스트를 정렬하는 함수
+   * @returns 정렬된 메달 리스
+   */
+  const getSortedMedals = () => {
+    if (sortType === "gold") {
+      const sortedMedalList = [...medalList].sort((a, b) => {
+        return b.goldMedalCount - a.goldMedalCount;
+      });
+      return sortedMedalList;
+    } else {
+      const sortedMedalList = [...medalList].sort((a, b) => {
+        return (
+          b.goldMedalCount +
+          b.silverMedalCount +
+          b.bronzeMedalCount -
+          (a.goldMedalCount + a.silverMedalCount + a.bronzeMedalCount)
+        );
+      });
+      return sortedMedalList;
+    }
+  };
+
+  const sortedMedalList = getSortedMedals();
+
+  /** 메달 리스트에 들어있는 값 확인 */
   const check = medalList.length === 0;
 
   /** UI */
   return (
     <main>
       <MedalForm medalList={medalList} setMedalList={setMedalList}></MedalForm>
-      <RadioButton
-        medalList={medalList}
-        setMedalList={setMedalList}
-      ></RadioButton>
+      <RadioButton sortType={sortType} setSortType={setSortType}></RadioButton>
       {check && <p>아직 추적된 국가가 없습니다. 메달을 추적하세요🥇🥈🥉</p>}
       {check || (
         <div className="medal-list">
           <MedalList
-            medalList={medalList}
+            medalList={sortedMedalList}
             setMedalList={setMedalList}
           ></MedalList>
         </div>
--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,31 +1,6 @@
 import React from "react";
-import { useState } from "react";
 
-const RadioButton = ({ medalList, setMedalList }) => {
-  /** 금메달 기준 정렬 */
-  const goldMedalLank = () => {
-    setMedalList(
-      [...medalList].sort((a, b) => {
-        return b.goldMedalCount - a.goldMedalCount;
-      })
-    );
-  };
-
-  /** 총 메달 수 기준 정렬 */
-  const totalMedalLank = () => {
-    const totalMedalList = medalList.map((country) => {
-      const { goldMedalCount, silverMedalCount, bronzeMedalCount } = country;
-      const total = goldMedalCount + silverMedalCount + bronzeMedalCount;
-      return { ...country, totalMedal: total };
-    });
-
-    setMedalList(
-      totalMedalList.sort((a, b) => {
-        return b.totalMedal - a.totalMedal;
-      })
-    );
-  };
-
+const RadioButton = ({ sortType, setSortType }) => {
   /** Radio 버튼 UI */
   return (
     <div className="radio-button">
@@ -33,8 +8,9 @@ const RadioButton = ({ medalList, setMedalList }) => {
         type="radio"
         name="sorting"
         id="gold"
-        value={1}
-        onClick={goldMedalLank}
+        value="gold"
+        checked={sortType === "gold"}
+        onChange={(e) => setSortType(e.target.value)}
       />{" "}
       <label htmlFor="gold" className="gold-lank">
         금메달순
@@ -43,8 +19,13 @@ const RadioButton = ({ medalList, setMedalList }) => {
         type="radio"
         name="sorting"
         id="total"
-        value={2}
-        onClick={totalMedalLank}
+        value="total"
+        checked={sortType === "total"}
+        onChange={(e) => {
+          console.log(e.target.value);
+
+          setSortType(e.target.value);
+        }}
       />{" "}
       <label htmlFor="total" className="total-lank">
         합계순
